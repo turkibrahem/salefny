5041266a3af4e611769d43b722faf30f7c45785a
Muhammed Essa Hameed <muhammed@fedora.muhammed>
2017-09-04 00:17:56 +0300

    final project

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,28 +4,10 @@
             @section('content')
            
            
-
-
- <form>
-  <div class="form-group">
-    <label for="email">Email address:</label>
-    <input type="email" class="form-control" id="email">
-  </div>
-  <div class="form-group">
-    <label for="pwd">Password:</label>
-    <input type="password" class="form-control" id="pwd">
-  </div>
-  <div class="checkbox">
-    <label><input type="checkbox"> Remember me</label>
-  </div>
-  <button type="submit" class="btn btn-default">Submit</button>
-</form> 
-
-
-
-
-
-
+{!! Form::open(['url' => 'foo/bar']) !!}
+ 
+{!! Form::close() !!}
+ 
                 
  {{--  {!! Form::open(['action' => 'PostsController@store', 'method'=>'POST','enctype'=>'multipart/form-data']) !!}
    
