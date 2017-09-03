21f152f92d4fcc6017dc036b2dc5cee1eab964fb
Muhammed Essa Hameed <muhammed@fedora.muhammed>
2017-09-04 00:22:00 +0300

    final project

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,6 +6,37 @@
            
 {!! Form::open(['action' => 'PostsController@store', 'method'=>'POST','enctype'=>'multipart/form-data']) !!}
  
+
+   <div class=container>
+<div class="form-group">
+      {{Form::label('subject', 'Subject')}}
+      {{Form::text('subject','',['class'=>'form-control','placeholder'=>'Subject']) }}
+    </div>
+<div class="form-group">
+      {{Form::label('firstname', 'First name')}}
+      {{Form::text('firstname','',['class'=>'form-control','placeholder'=>'First name']) }}
+    </div>
+
+<div class="form-group">
+      {{Form::label('lastname', 'Last name')}}
+      {{Form::text('lastname','',['class'=>'form-control','placeholder'=>'Last name']) }}
+    </div>
+
+<div class="form-group">
+      {{Form::label('body', 'Discriptions')}}
+      {{Form::textarea('body','',['class'=>'form-control','placeholder'=>'Discriptions','id'=>'article-ckeditor']) }}
+    </div>
+
+<div class="form-group">
+      
+      {{Form::file('post_image','',['class'=>'form-control' ]) }}
+    </div> 
+
+{{Form::submit('Create',['class'=>"btn btn-primary btn-lg"]) }}
+
+   </div>
+
+
 {!! Form::close() !!}
  
                 
